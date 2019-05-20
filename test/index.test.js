611f9a8cdad9ecbc3320d5f40b71d5c0f3d69f7f
LeonFedotov <leonf@wix.com>
2019-05-20 19:02:49 +0300

    cleanup

--- test/index.test.js
+++ test/index.test.js
@@ -81,32 +81,3 @@ describe('fish footman', () => {
     await probot.receive({ name: 'issues', payload: fixtures.issueEdited })
   })
 })
-
-// describe('Fish footman', () => {
-//   let probot
-
-//   beforeEach(() => {
-//     probot = new Probot({})
-//     const app = probot.load(myProbotApp)
-//     app.app = () => 'test'
-//   })
-
-//   test('creates a comment when an issue is opened', async () => {
-//     // Test that we correctly return a test token
-//     nock('https://api.github.com')
-//       .post('/app/installations/2/access_tokens')
-//       .reply(200, { token: 'test' })
-
-//     // Test that a comment is posted
-//     nock('https://api.github.com')
-//       .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
-//         expect(body).toMatchObject(issueCreatedBody)
-//         return true
-//       })
-//       .reply(200)
-
-//     // Receive a webhook event
-//     await probot.receive({ name: 'issues', payload })
-//   })
-// })
-
