3c3cd7b90fe3ad49389c9bea6e3fb666282ab6af
Glitch (intriguing-penguin) <none>
2019-05-21 08:55:34 +0000

    Import of LeonFedotov/fish-footman

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+
+upload_files() {
+  git remote add production https://${GLICH_TOKEN}@api.glitch.com/git/fish-footman > /dev/null 2>&1
+  git push -f --quiet --set-upstream production master 
+}
+
+setup_git
+upload_files
