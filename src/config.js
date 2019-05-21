1e0751a67430502afa74bc934191182ce29db4c0
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2019-05-21 10:32:41 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  prTrigger: { opened: true, edited: true, reopened: true, unlocked: true, syncronize: true },
-  issueTrigger: { labeled: true },
   labelName: 'Fishy',
   statusName: 'Directory Locks',
   statusDescription: 'Checking mergeability'
