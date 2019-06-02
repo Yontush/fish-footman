7cb3eafa23d9f5a312323f650044396cc16cbc86
LeonFedotov <leonf@wix.com>
2019-06-02 12:23:43 +0300

    removing flags, using functions instead...

--- src/limitMerge.js
+++ src/limitMerge.js
@@ -1,4 +1,13 @@
 const { createStatus, restrictedDirs, pullrequests } = require('./lib')
+const validatePr = async (context, pr, restrictions) => {
+  await createStatus(context, pr.sha, 'pending')
+  for await (const file of pr.files) {
+    if (restrictions.some((dir) => file.startsWith(dir))) {
+      return await createStatus(context, pr.sha, 'failure')
+    }
+  }
+  return await createStatus(context, pr.sha, 'success')
+}
 
 module.exports = async (context) => {
   context.log('Gathering restrictions')
@@ -6,19 +15,6 @@ module.exports = async (context) => {
   context.log(`Got ${restrictions.length} restrictions`)
 
   for await (const pr of pullrequests(context)) {
-    context.log(`Validating PR #${pr.number}`)
-    let valid = true
-    await createStatus(context, pr.sha, 'pending')
-    for await (const file of pr.files) {
-      if (restrictions.some((dir) => file.startsWith(dir))) {
-        context.log(`PR #${pr.number} invalid`)
-        await createStatus(context, pr.sha, 'failure')
-        valid = false
-        break
-      }
-      context.log(`.`)
-    }
-    valid && context.log(`PR #${pr.number} valid`)
-    valid && await createStatus(context, pr.sha, 'success')
+    await validatePr(context, pr, restrictions)
   }
 }
